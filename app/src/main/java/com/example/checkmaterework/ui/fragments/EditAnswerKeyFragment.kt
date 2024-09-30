7059c55147e3760d7c66713d71857997cb4fbab7
Emman <esplana713@gmail.com>
2024-09-30 15:37:01 +0800

    added the layout for editing key for word problems

--- app/src/main/java/com/example/checkmaterework/ui/fragments/EditAnswerKeyFragment.kt
+++ app/src/main/java/com/example/checkmaterework/ui/fragments/EditAnswerKeyFragment.kt
@@ -45,20 +45,20 @@ class EditAnswerKeyFragment(private val answerSheet: AnswerSheetEntity) : Fragme
             if (examType == "Multiple Choice") {
                 for (i in 1..itemCount) {
                     // Create horizontal LinearLayout for each question
-                    val questionLayout = LinearLayout(requireContext()).apply {
-                        orientation = LinearLayout.HORIZONTAL
-                        layoutParams = LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT
-                        )
-                    }
+//                    val questionLayout = LinearLayout(requireContext()).apply {
+//                        orientation = LinearLayout.HORIZONTAL
+//                        layoutParams = LinearLayout.LayoutParams(
+//                            LinearLayout.LayoutParams.MATCH_PARENT,
+//                            LinearLayout.LayoutParams.WRAP_CONTENT
+//                        )
+//                    }
 
                     // Create TextView for numbering
                     val numberTextView = TextView(requireContext()).apply {
                         text = "$currentItemNumber: "
                         textSize = 20f // Adjust text size as needed
                     }
-                    questionLayout.addView(numberTextView)
+                    answerKeyContainer.addView(numberTextView)
 
                     // Create ChipGroup for multiple-choice
                     val chipGroup = ChipGroup(requireContext()).apply {
@@ -70,10 +70,10 @@ class EditAnswerKeyFragment(private val answerSheet: AnswerSheetEntity) : Fragme
                             isCheckable = true
                         })
                     }
-                    questionLayout.addView(chipGroup)
+                    answerKeyContainer.addView(chipGroup)
 
-                    // Add the horizontal LinearLayout to the answerKeyContainer
-                    answerKeyContainer.addView(questionLayout)
+//                    // Add the horizontal LinearLayout to the answerKeyContainer
+//                    answerKeyContainer.addView(questionLayout)
 
                     currentItemNumber++ // Increment item number
                 }
@@ -90,38 +90,95 @@ class EditAnswerKeyFragment(private val answerSheet: AnswerSheetEntity) : Fragme
 
                     // Create TextView for numbering
                     val numberTextView = TextView(requireContext()).apply {
-                        text = "$currentItemNumber: "
+                        text = "$currentItemNumber : "
                         textSize = 20f // Adjust text size as needed
                     }
                     questionLayout.addView(numberTextView)
 
                     // Create TextInputLayout for identification
-                    val textInputLayout = TextInputLayout(requireContext()).apply {
-                        hint = "Enter answer for item $currentItemNumber"
-                        layoutParams = LinearLayout.LayoutParams(
-                            0, // Set width to 0dp
-                            LinearLayout.LayoutParams.WRAP_CONTENT
-                        ).apply {
-                            weight = 1f // Set weight to 1 to take up remaining space
-                        }
-                        visibility = View.VISIBLE
+                    val identificationLayout = createTextInputLayout("Answer for $currentItemNumber")
 
-                        // Add TextInputEditText to TextInputLayout
-                        val textInputEditText = TextInputEditText(requireContext())
-//                        textInputEditText.hint = "Enter answer for item $i"
-                        hint = "Enter answer for item $currentItemNumber"
-
-                        addView(textInputEditText)
-                    }
-                    questionLayout.addView(textInputLayout)
+//                    val textInputLayout = TextInputLayout(requireContext()).apply {
+//                        hint = "Answer for $currentItemNumber"
+//                        layoutParams = LinearLayout.LayoutParams(
+//                            0, // Set width to 0dp
+//                            LinearLayout.LayoutParams.WRAP_CONTENT
+//                        ).apply {
+//                            weight = 1f // Set weight to 1 to take up remaining space
+//                        }
+//                        visibility = View.VISIBLE
+//
+//                        // Add TextInputEditText to TextInputLayout
+//                        val textInputEditText = TextInputEditText(requireContext())
+//                        hint = "Answer for $currentItemNumber"
+//
+//                        addView(textInputEditText)
+//                    }
+                    questionLayout.addView(identificationLayout)
 
                     // Add the horizontal LinearLayout to the answerKeyContainer
                     answerKeyContainer.addView(questionLayout)
 
                     currentItemNumber ++ // Increment the item number for the next iteration
                 }
+            } else if (examType == "Word Problem") {
+                // Calculate the number of word problems
+                val wordProblemsCount = itemCount / 5
+
+                for (i in 1..wordProblemsCount) {
+                    val questionLayout = LinearLayout(requireContext()).apply {
+                        orientation = LinearLayout.VERTICAL // Change to vertical orientation
+                        layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT
+                        )
+                    }
+
+                    val wordProblemStart = currentItemNumber
+                    val wordProblemEnd = currentItemNumber + 4
+
+                    val numberTextView = TextView(requireContext()).apply {
+                        text = "$wordProblemStart - $wordProblemEnd : "
+                        textSize = 20f
+                    }
+                    questionLayout.addView(numberTextView)
+
+                    // Create layouts for Asked, Given, Operation, Number Sentence, Solution/Answer
+                    val askedLayout = createTextInputLayout("Asked")
+                    val givenLayout =createTextInputLayout("Given")
+                    val operationLayout = createTextInputLayout("Operation")
+                    val numberSentenceLayout = createTextInputLayout("Number Sentence")
+                    val solutionLayout = createTextInputLayout("Solution/Answer")
+
+                    // Add layouts to questionLayout
+                    questionLayout.addView(askedLayout)
+                    questionLayout.addView(givenLayout)
+                    questionLayout.addView(operationLayout)
+                    questionLayout.addView(numberSentenceLayout)
+                    questionLayout.addView(solutionLayout)
+
+                    answerKeyContainer.addView(questionLayout)
+
+                    currentItemNumber += 5 // Increment item number for the next iteration
+                }
             }
         }
+    }
+
+    private fun createTextInputLayout(hint: String): TextInputLayout {
+        return TextInputLayout(requireContext()).apply {
+            this.hint = hint
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            visibility = View.VISIBLE
+
+            val textInputEditText = TextInputEditText(requireContext())
+            this.hint = hint
+            addView(textInputEditText)
+        }
+
     }
 
     override fun getFragmentTitle(): String {
